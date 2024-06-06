8365109b1f227b07998833de6c9f78172103a2d6
Anjali Chaturvedi <anjaliichaturvedi@gmail.com>
2024-06-06 22:24:51 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from "react";
-import Cursor from "./components/cursor";
-import "./App.css";
 import {
   ChakraProvider,
   extendTheme,
@@ -11,330 +9,41 @@ import {
   useColorMode,
   ColorModeProvider,
 } from "@chakra-ui/react";
-import { Link, animateScroll as scroll } from "react-scroll";
-import { Tooltip } from "@chakra-ui/react";
-import { SunIcon, MoonIcon } from "@chakra-ui/icons";
-import { Button } from "@chakra-ui/react";
+import Navbar from './components/Navbar';
+import LandingPage from './components/LandingPage';
+import About from './components/About';
+import Program from './components/Program';
+import Team from './components/Team';
+import Sponsor from './components/PastSponsors';
+import Upcoming from './components/Upcoming';
+import GetInvolvedSection from './components/GetInvolved';
+import Footer from './components/Footer';
+
 
 function App() {
-  const scrollToTop = () => {
-    scroll.scrollToTop({
-      duration: 500, // Adjust the scroll duration as needed
-      smooth: "easeInOutQuad", // Adjust the scroll animation as needed
-    });
-  };
+  // Define the dark theme
+const darkTheme = extendTheme({
+  config: {
+    initialColorMode: "dark",
+    useSystemColorMode: false,
+  },
+});
 
-  const [theme, setTheme] = useState("dark");
-  const toggleTheme = () => {
-    if (theme === "light") {
-      setTheme("dark");
-    } else {
-      setTheme("light");
-    }
-  };
-  useEffect(() => {
-    document.body.className = theme;
-  }, [theme]);
-
-  const scrollToSection = (id) => {
-    const element = document.getElementById(id);
-    if (element) {
-      element.scrollIntoView({ behavior: "smooth" });
-    }
-  };
-  const [scaling, setscaling] = useState(false);
   return (
-    <div className={`App ${theme}`}>
-      <ChakraProvider theme={theme}>
-        <ColorModeProvider>
-          <div className="App">
-            <header className="App-header">
-              <div className="content-container">
-                <div className="left-aligned">
-                  {/* Navigation Bar */}
-                  <Flex
-                    as="nav"
-                    align="center"
-                    justify="space-between"
-                    color="rgb(170, 170, 170);
-                "
-                    className="link"
-                  >
-                    {/* Title/Brand */}
-                    <Link
-                      to="project-section"
-                      smooth={true}
-                      duration={500}
-                      spy={true}
-                      offset={-70} // Adjust this value to match your layout
-                      activeClass="active"
-                      className="project-header"
-                    >
-                      Projects
-                    </Link>
-
-                    {/* Navigation Links */}
-                    <Flex align="center">
-                      <IconButton
-                        className="custom-icon-button" // Apply the custom class
-                        icon={theme === "dark" ? <MoonIcon /> : <SunIcon />}
-                        onClick={toggleTheme}
-                        aria-label="Toggle Theme"
-                      />
-                    </Flex>
-                  </Flex>
-                  <h1>Hi, I am Anjali Chaturvedi</h1>
-                  <p>
-                    I am passionate about using technology as an enabler for
-                    empowering organizations, communities and individuals.
-                  </p>
-
-                  {/* Other content goes here */}
-
-                  <div className="resume-download">
-                    <a
-                      href="files/Resume_Anjali_Chaturvedi_Summer.pdf"
-                      download="Resume_Anjali_Chaturvedi.pdf"
-                    >
-                      Download Resume →
-                    </a>
-                  </div>
-                  {/* Recently Section */}
-                  <div className="recently-section">
-                    <div className="recently-heading">
-                      <hr className="recently-hr" />
-                      <h2>Recently</h2>
-                    </div>
-                    <ul className="recently-list">
-                      <li> 
-                      Organizing a 10-week mentorship initiative within {""}
-                        <a
-                          target="_blank"
-                          href="https://www.linkedin.com/company/nodebrew"
-                          className="company-link"
-                        >
-                          Nodebrew's
-                        </a>{" "} community, driving unparalleled growth and skill development
-                      </li>  {/* Recently Section 
-                      <li>
-                      Implementing innovative strategies to captivate audiences and broaden reach for {" "}
-                        <a
-                          target="_blank"
-                          href="https://linkedin.com/company/skillarena"
-                          className="company-link"
-                        >
-                          {" "}
-                          Skillarena{" "}
-                        </a> as their Summer Associate
-                      </li> */}
-                      <li>
-                      Empowering aspiring technologists at {" "}
-                        <a
-                          target="_blank"
-                          href="https://linkedin.com/company/codesscafe"
-                          className="company-link"
-                        >
-                          {" "}
-                          Codess Cafe{" "}
-                        </a> through personalized mentorship in the realm of open source
-                      </li> 
-                      <li>
-                      Creating a cutting-edge curriculum for mastering technical interviews and conquering complex coding challenges  {" "}
-                        <a
-                          target="_blank"
-                          href="https://linkedin.com/company/codepath"
-                          className="company-link"
-                        >
-                          {" "}
-                          CodePath{" "}
-                        </a> 
-                      </li> 
-                    </ul>
-                  </div>
-                  {/* Projects Section */}
-                  <div className="projects-section" id="project-section">
-                    <hr className="projects-hr" />
-                    <h2>Projects</h2>
-                    <div className="project-item">
-                      <h3>
-                        Internet Corner (this website)
-                        <span className="project-tag">Project</span>
-                      </h3>
-                      <p>
-                        A React-powered personal website featuring project
-                        showcases and a resume, designed with precision to
-                        deliver an engaging and dynamic user experience.
-                      </p>
-                      <p className="project-year">2024</p>
-                      <a
-                        href="https://github.com/anjalichaturvedi/anjalichaturvedi.github.io"
-                        className="view-project-link"
-                      >
-                        View Source Code →
-                      </a>
-                    </div>
-                    <div className="project-item">
-                      <h3>
-                        Campus Navigator
-                        <span className="project-tag">Project</span>
-                      </h3>
-                      <p>
-                      A user-centric web application developed in JavaScript and Leaflet, poised to redefine accessibility and convenience for students, faculty, and visitors alike.
-                      </p>
-                      <p className="project-year">2024</p>
-                      <a
-                        href="https://github.com/anjalichaturvedi/campus-navigator"
-                        className="view-project-link"
-                      >
-                        View Source Code →
-                      </a>
-                    </div>
-                    <div className="project-item">
-                      <h3>
-                        Talent Track
-                        <span className="project-tag">Project</span>
-                      </h3>
-                      <p>
-                        Python and SQL-based application to streamline the
-                        process of analyzing, storing, and comparing candidate
-                        data.
-                      </p>
-                      <p className="project-year">2024</p>
-                      <a
-                        href="https://github.com/anjalichaturvedi/talent-track"
-                        className="view-project-link"
-                      >
-                        View Source Code →
-                      </a>
-                    </div>
-                    <div className="project-item">
-                      <h3>
-                        Smart Scheduler
-                        <span className="project-tag">Project</span>
-                      </h3>
-                      <p>
-                        A web application developed in PHP, SQL, HTML/CSS, and
-                        JavaScript, providing a user-friendly platform for
-                        administrators, teachers, and students to efficiently
-                        manage and view timetables in educational institutions.
-                      </p>
-                      <p className="project-year">2023</p>
-                      <a
-                        target="_blank"
-                        href="https://smart-scheduler.000webhostapp.com/index.php"
-                        className="view-project-link"
-                      >
-                        View Project →
-                      </a>
-                    </div>
-                    <div className="project-item">
-                      <h3>
-                        Markdown Generator
-                        <span className="project-tag">Mini Project</span>
-                      </h3>
-                      <p>
-                        React-based web application for effortlessly creating,
-                        editing, and generating Markdown tables with a
-                        user-friendly interface.
-                      </p>
-                      <p className="project-year">2023</p>
-                      <a
-                        target="_blank"
-                        href="https://markdown-generator-phi.vercel.app"
-                        className="view-project-link"
-                      >
-                        View Project →
-                      </a>
-                    </div>
-                    <div className="project-item">
-                      <h3>
-                        Password Generator
-                        <span className="project-tag">Mini Project</span>
-                      </h3>
-                      <p>
-                        React-based password generator built with Chakra UI
-                        allows users to customize and generate secure passwords
-                        based on their preferences.
-                      </p>
-                      <p className="project-year">2023</p>
-                      <a
-                        target="_blank"
-                        href="https://password-generator-rust-kappa.vercel.app"
-                        className="view-project-link"
-                      >
-                        View Project →
-                      </a>
-                    </div>
-                    <div className="project-item">
-                      <h3>
-                        Blood Bank System
-                        <span className="project-tag">Mini Project</span>
-                      </h3>
-                      <p>
-                        A blood bank management system created with PHP and SQL,
-                        connecting users and hospitals for seamless blood
-                        donation coordination and efficient inventory
-                        management.
-                      </p>
-                      <p className="project-year">2023</p>
-                      <a
-                        target="_blank"
-                        href="https://github.com/anjalichaturvedi/template-blood-bank"
-                        className="view-project-link"
-                      >
-                        View Source Code →
-                      </a>
-                    </div>
-                  </div>
-
-                  <hr className="spacer-hr" />
-                  <div className="footer">
-                    <div className="footer-columns">
-                      <div className="footer-column">
-                        <p>© Anjali Chaturvedi</p>
-                        {/* Add more content for the first column as needed */}
-                      </div>
-                      <div className="footer-column1">
-                        <p>
-                          <a href="https://www.github.com/anjalichaturvedi">
-                            {" "}
-                            GitHub
-                          </a>
-                          <a
-                            href="https://www.linkedin.com/in/anjali-chaturvedi"
-                            style={{ marginLeft: "15px" }}
-                          >
-                            {""}
-                            LinkedIn{""}
-                          </a>
-                          <a
-                            href="mailto:contact@anjalichaturvedi.tech"
-                            style={{ marginLeft: "15px" }}
-                          >
-                            {""}
-                            Email{""}
-                          </a>
-                        </p>
-                        {/* Add more content for the second column as needed */}
-                      </div>
-                    </div>
-                    <Tooltip>
-                      <Button
-                        className="back-to-top-button"
-                        onClick={scrollToTop}
-                        size="md"
-                      >
-                        Back to Top ↑
-                      </Button>
-                    </Tooltip>
-                  </div>
-                </div>
-              </div>
-            </header>
-          </div>
-        </ColorModeProvider>
-      </ChakraProvider>
-    </div>
+    <ChakraProvider theme={darkTheme}>
+      <Box minHeight="100vh">
+        <Navbar />
+        <LandingPage />
+        < About />
+        < Program />
+        < Upcoming />
+        < GetInvolvedSection />
+        < Sponsor />
+        < Team />
+        < Footer />
+        {/* Your other components and routes go here */}
+      </Box>
+    </ChakraProvider>
   );
 }
 
